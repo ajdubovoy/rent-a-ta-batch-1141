82a16c36c2ec25df2803b05f51f339a7daf4d42d
Alexander Dubovoy <ajdubovoy@gmail.com>
2023-02-28 16:47:54 +0100

    make the models

---
+++ db/migrate/20230228154417_create_teachers.rb
@@ -0,0 +1,13 @@
+class CreateTeachers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.string :experience
+      t.integer :batch_number
+      t.string :superpower
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
